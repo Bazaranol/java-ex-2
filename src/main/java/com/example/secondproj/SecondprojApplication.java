d4937c7dcca2c72706b457c56b8f2e80031ce3f9
Bazaranol <romansidorkin2@gmail.com>
2024-12-24 17:59:00 +0700

    init

---
+++ src/main/java/com/example/secondproj/SecondprojApplication.java
@@ -0,0 +1,13 @@
+package com.example.secondproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondprojApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondprojApplication.class, args);
+	}
+
+}
